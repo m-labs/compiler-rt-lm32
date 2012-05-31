12038eae65b760f2ee3fd37181b95e4fac12320c
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-05-31 20:48:25 +0200

    lm32: add divdi3 ashrdi3 udivmoddi4

--- make/platform/lm32.mk
+++ make/platform/lm32.mk
@@ -7,5 +7,5 @@ CC := clang
 
 CFLAGS := -Wall -Werror -O3 -D_YUGA_LITTLE_ENDIAN=0 -D_YUGA_BIG_ENDIAN=1 -ccc-host-triple lm32-elf
 
-FUNCTIONS := divsi3 modsi3 comparedf2 negsf2 negdf2 addsf3 subsf3 mulsf3 divsf3 lshrdi3 muldi3 ashldi3 \
+FUNCTIONS := divsi3 modsi3 comparedf2 negsf2 negdf2 addsf3 subsf3 mulsf3 divsf3 lshrdi3 muldi3 divdi3 ashldi3 ashrdi3 udivmoddi4 \
   floatsisf floatunsisf fixsfsi fixunssfsi adddf3 subdf3 muldf3 divdf3 floatsidf floatunsidf fixdfsi fixunsdfsi
