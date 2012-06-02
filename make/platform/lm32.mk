cdc51eff51bb7cbd2cdeb03e5bedafb556a31d67
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-06-03 00:16:37 +0200

    lm32: add floatdidf

--- make/platform/lm32.mk
+++ make/platform/lm32.mk
@@ -8,4 +8,4 @@ CC := clang
 CFLAGS := -Wall -Werror -O3 -D_YUGA_LITTLE_ENDIAN=0 -D_YUGA_BIG_ENDIAN=1 -ccc-host-triple lm32-elf
 
 FUNCTIONS := divsi3 modsi3 comparedf2 negsf2 negdf2 addsf3 subsf3 mulsf3 divsf3 lshrdi3 muldi3 divdi3 ashldi3 ashrdi3 udivmoddi4 \
-  floatsisf floatunsisf fixsfsi fixunssfsi adddf3 subdf3 muldf3 divdf3 floatsidf floatunsidf fixdfsi fixunsdfsi
+  floatsisf floatunsisf fixsfsi fixunssfsi adddf3 subdf3 muldf3 divdf3 floatsidf floatunsidf floatdidf fixdfsi fixunsdfsi
